870dfd16d36a7f3355f3f7ba749a9775b1c85ce7
AmbrishProject <ambreesh09tripathi@gmail.com>
2023-11-01 11:36:16 +0530

    images

---
+++ lib/utils/images_strings.dart
@@ -0,0 +1,3 @@
+const String tonBoardingImage1 = "images/on_boarding_images/on1.png";
+const String tonBoardingImage2 = "images/on_boarding_images/on2.png";
+const String tonBoardingImage3 = "images/on_boarding_images/on3.png";
---
+++ lib/utils/text_strings.dart
@@ -0,0 +1,6 @@
+const String tonBoardingTitle1 = "Taste the world, effortlessly";
+const String tonBoardingTitle2 = "Ignite your taste bud revolution";
+const String tonBoardingTitle3 = "Embrace your foodie journey!";
+const String tonBoardingCounter1 = "1";
+const String tonBoardingCounter2 = "2";
+const String tonBoardingCounter3 = "3";
